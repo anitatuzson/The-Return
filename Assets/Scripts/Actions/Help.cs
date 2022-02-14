8a68aeac1339de60a8a22b76749f4c09b7a05789
Anita <tuzson.anita@gmail.com>
2022-02-14 15:34:37 +0200

    Initial commit

---
+++ Assets/Scripts/Actions/Help.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Actions/Help")]
+public class Help : Action
+{
+    
+    public override void RespondToInput(GameController controller, string verb)
+    {
+        controller.currentText.text = "Type a Verb followed by a noun(e.g. \"go north\")";
+        controller.currentText.text += "\nAllowed verbs:\nGo, Examine, Get, Use, Inventory, TalkTo, Say, Give, Read, Help";
+    }
+}
